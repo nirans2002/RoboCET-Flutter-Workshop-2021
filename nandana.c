a43a6df5952bce7754bd5b3eea4004b950a1e6b8
nandanapm <pmnandana4@gmail.com>
2021-07-05 23:17:40 +0530

    file added

---
+++ nandana.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    int a,b,c;
+    printf("enter a value for a:");
+    scanf("%d",&a);
+    printf("enter a value for b:");
+    scanf("%d",&b);
+    c= a+b;
+    printf("the sum of %d and %d is equal to %d",a,b,c);
+}
